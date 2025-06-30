b89c01a598b8d47eca675c1957eaa3d64bfe7d1d
Cal Monica Chen <monicachen808@gmail.com>
2025-06-30 15:00:39 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Getting Started
+Install the dependencies and run the project
+```
+npm install
+npm start
+```
+unrusting my brain from coding w this mini project
---
+++ index.js
@@ -0,0 +1,11 @@
+let choresArray = []
+let cost = 0
+let carButton = document.getElementById("carButton")
+let lawnButton = document.getElementById("lawnButton")
+let weedButton = document.getElementById("weedButton")
+
+function sendInvoice(){
+    cost = 0
+    choresArray = []
+    //clear the textContent as well
+}
